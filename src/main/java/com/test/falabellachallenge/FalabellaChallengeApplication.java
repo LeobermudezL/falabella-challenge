f588f3900e8b454f3ba54eade7c9fa60573aef7e
AzureAD\EleonayBermudez <eleonay.bermudez@ibm.com>
2021-10-12 21:33:58 -0300

    first commit

---
+++ src/main/java/com/test/falabellachallenge/FalabellaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.test.falabellachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalabellaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalabellaChallengeApplication.class, args);
+	}
+
+}
